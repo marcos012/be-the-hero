dafc5d115f0884fe065cff6144483c6f82047aa5
marcos012 <marcospaulo.ad@gmail.com>
2020-03-24 23:22:31 -0300

    adicina rotas de post e get para ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const cryoto = require('crypto');
+const connection = require('./database/connection')
 
 const routes = express.Router();
 
-routes.get('/users/:id', (req, res) => {
-    const params = req.params;
+routes.get('/ongs', async (req, res) => {
+    const ongs = await connection('ongs').select('*');
 
-    return res.json({event: 'a'})
+    return res.json(ongs);
+});
+
+routes.post('/ongs', async (req, res) => {
+    const { name, email, whatsapp, city, uf } = req.body;
+
+    const id = cryoto.randomBytes(4).toString('HEX');
+    
+    await connection('ongs').insert({id, name, email, whatsapp, city, uf });
+
+    return res.json({ id });
 });
 
 module.exports = routes;
