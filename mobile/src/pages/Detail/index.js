7518f4053ebb9945248ce23b51e73ffbf9c837b7
marcos012 <marcospaulo.ad@gmail.com>
2020-03-26 21:49:06 -0300

    integrando com api

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Image, TouchableOpacity, Text, Linking } from 'react-native';
 import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import * as MailComposer from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
@@ -9,7 +9,10 @@ import styles from './styles';
 
 const Details = () => {
     const navigation = useNavigation();
-    const message = 'Olá, estou entrando em contato pois gostaria de ajudar no caso "a" com o valor de R$120,00';
+    const route = useRoute();
+
+    const incident = route.params.incident;
+    const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso ${incident.title} com o valor de ${Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}`;
 
     const navigateBack = () => {
         navigation.goBack();
@@ -17,14 +20,14 @@ const Details = () => {
 
     const sendMail = () => {
         MailComposer.composeAsync({
-            subject: 's',
-            recipients: ['aaaa'],
+            subject: `Herói do caso: ${incident.title}`,
+            recipients: [incident.email],
             body: message
         })
     }
 
     const sendWhatsapp = () => {
-        Linking.openURL(`whatsapp://send?phone=4444444444&text=${message}`)
+        Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`)
     }
     return (
         <View style={styles.container}>
@@ -37,13 +40,15 @@ const Details = () => {
 
             <View style={styles.incident}>
                 <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
-                <Text style={styles.incidentValue}>AAAAAAA</Text>
+                <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
 
                 <Text style={styles.incidentProperty}>CASO:</Text>
-                <Text style={styles.incidentValue}>bbbb</Text>
+                <Text style={styles.incidentValue}>{incident.title}</Text>
 
                 <Text style={styles.incidentProperty}>VALOR:</Text>
-                <Text style={styles.incidentValue}>R$ 120,00</Text>
+                <Text style={styles.incidentValue}>
+                    {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}
+                </Text>
             </View>
 
             <View style={styles.contactBox}>
--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,23 +1,39 @@
- import React from 'react';
+ import React, { useEffect, useState } from 'react';
  import { Feather } from '@expo/vector-icons';
  import { useNavigation } from '@react-navigation/native';
  import { View, Image, Text, TouchableOpacity, FlatList } from 'react-native';
 
  import logoImg from '../../assets/logo.png';
  import styles from './styles';
+ import api from '../../services/api';
 
  const Incidents = () => {
-     const navigation = useNavigation();
+    const [incidents, setIncidents] = useState([]);
+    const [total, setTotal] = useState(0);
 
-     const navigateToDetail = () => {
-         navigation.navigate('Detail');
-     }
-     return (
+    const navigation = useNavigation();
+
+    const navigateToDetail = (incident) => {
+        navigation.navigate('Detail', { incident });
+    }
+    
+    async function loadIncidents() {
+        const response = await api.get('incidents');
+
+        setIncidents(response.data);
+        setTotal(response.headers['x-total-count'])
+    }
+
+    useEffect(() => {
+       loadIncidents();
+    }, [])
+
+    return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg} />
                 <Text style={styles.headerText}>
-                    Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+                    Total de <Text style={styles.headerTextBold}>{total} casos</Text>.
                 </Text>
             </View>
 
@@ -26,28 +42,30 @@
 
             <FlatList 
                 style={styles.incidentList}
-                data={[1, 2, 3]}
-                keyExtractor={incident => String(incident)}
+                data={incidents}
+                keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
-                renderItem={() => (
+                renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
-                        <Text style={styles.incidentValue}>AAAAAAA</Text>
+                        <Text style={styles.incidentValue}>{incident.name}</Text>
 
                         <Text style={styles.incidentProperty}>CASO:</Text>
-                        <Text style={styles.incidentValue}>bbbb</Text>
+                        <Text style={styles.incidentValue}>{incident.title}</Text>
 
                         <Text style={styles.incidentProperty}>VALOR:</Text>
-                        <Text style={styles.incidentValue}>R$ 120,00</Text>
+                        <Text style={styles.incidentValue}>
+                            {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}
+                        </Text>
 
-                        <TouchableOpacity style={styles.detailsButton} onPress={navigateToDetail}>
+                        <TouchableOpacity style={styles.detailsButton} onPress={() => navigateToDetail(incident)}>
                             <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
                             <Feather name="arrow-right" size={16} color="#E02041"/>
                         </TouchableOpacity>
                     </View>
             )}/>
         </View>
-     );
+    );
  }
   
  export default Incidents;
